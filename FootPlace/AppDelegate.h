bd7ba693880537176ef46ea233d863c7cd39c976
wangchengyuan <wangchengyuan@wangchengyuandemacbook-pro.local>
2015-10-15 15:23:46 +0800

    Initial Commit

---
+++ FootPlace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FootPlace
+//
+//  Created by wangchengyuan on 15/10/15.
+//  Copyright © 2015年 wangchengyuan.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FootPlace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FootPlace
+//
+//  Created by wangchengyuan on 15/10/15.
+//  Copyright © 2015年 wangchengyuan.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
